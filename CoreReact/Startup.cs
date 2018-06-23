8606ea6cc7f7e91d87715b2eb92375687707a8af
Peska <peska_jr@o2.pl>
2018-06-23 22:06:33 +0200

    Just saving my progress

--- CoreReact/Startup.cs
+++ CoreReact/Startup.cs
@@ -1,34 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
+using System;
 
 namespace CoreReact
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddReact();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.AddMvc();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			return services.BuildServiceProvider();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseReact(config =>
+			{
+				// If you want to use server-side rendering of React components,
+				// add all the necessary JavaScript files here. This includes
+				// your components as well as all of their dependencies.
+				// See http://reactjs.net/ for more information. Example:
+				//config
+				//  .AddScript("~/Scripts/First.jsx")
+				//  .AddScript("~/Scripts/Second.jsx");
+
+				// If you use an external build too (for example, Babel, Webpack,
+				// Browserify or Gulp), you can improve performance by disabling
+				// ReactJS.NET's version of Babel and loading the pre-transpiled
+				// scripts. Example:
+				//config
+				//  .SetLoadBabel(false)
+				//  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+			});
+
+			app.UseStaticFiles();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
